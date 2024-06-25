265a0f76d67b372c2d320b815c6a1ba01b7b74f3
whitney-shisia <shisiawhitney215@gmail.com>
2024-06-25 12:40:44 +0300

    flask

--- server/models.py
+++ server/models.py
@@ -24,7 +24,7 @@ class Department(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    address = db.Column(db.String)
+    location = db.Column(db.String)
 
     def __repr__(self):
-        return f'<Department {self.id}, {self.name}, {self.address}>'
+        return f'<Department {self.id}, {self.name}, {self.location}>'
